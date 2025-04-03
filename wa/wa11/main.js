e6472e158946713bbb5b080f91d091294bc4104d
crhe4859 <89471477+crhe4859@users.noreply.github.com>
2025-04-03 15:28:01 -0600

    wa11 alt text update

--- wa/wa11/main.js
+++ wa/wa11/main.js
@@ -8,11 +8,11 @@ const overlay = document.querySelector('.overlay');
 
 const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
 const alts = {
-  'pic1.jpg' : 'Closeup of a human eye',
-  'pic2.jpg' : 'Rock that looks like a wave',
-  'pic3.jpg' : 'Purple and white pansies',
-  'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
-  'pic5.jpg' : 'Large moth on a leaf'
+  'pic1.jpg' : 'Fisheye lens cat',
+  'pic2.jpg' : 'Parrot being washed in sink}',
+  'pic3.jpg' : 'Fisheye lens cat 2',
+  'pic4.jpg' : 'Cat being flashbanged',
+  'pic5.jpg' : 'Alien'
 }
 
 /* Looping through images */
